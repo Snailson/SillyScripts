4557a630c4ef297db6924b28072a44f71bb23bb3
Snailson <chad.snailson@gmail.com>
2021-01-03 15:16:05 -0700

    Update DonutsQuick.ps1

--- Emails/DonutsQuick.ps1
+++ Emails/DonutsQuick.ps1
@@ -1,7 +1,6 @@
 $Outlook = New-Object -ComObject Outlook.Application
 $Mail = $Outlook.CreateItem(0)
 $Mail.To = "chad@wavetronix.com"
-$Mail.Subject = "Donuts3!"
+$Mail.Subject = "Donuts4!"
 $Mail.Body ="I'm pleased to announce that I'm bringing fresh donuts for everyone next time I'm in the office."
 $Mail.Send()
-exit
